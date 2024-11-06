861a6fcbea5a67db354cb38e865f40ede107f5bc
Arman Hossain <armanmahim00@gmail.com>
2024-11-06 20:18:39 +0600

    start commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+import Header from './components/header/Header'
+import Blogs from './components/Blogs/Blogs'
+function App() {
+  
+
+  return (
+    <>
+    
+      <Header></Header>
+      <Blogs></Blogs>
+      
+    </>
+  )
+}
+
+export default App
